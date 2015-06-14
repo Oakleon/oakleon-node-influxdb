1b569896d9d84cfd8e229ae7b5ee8b014636dc80
Tim Cash <timcash@gmail.com>
2015-06-14 16:08:13 -0700

    writing points works

--- src/index.js
+++ src/index.js
@@ -1,37 +1,80 @@
 import rp from "request-promise";
+//rp.debug = true;
+import lo from "lodash";
 
+// a point should look like
 
-export var put = function blah(endpoint, user, pass, points) {
-    rp(endpoint, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body) // Show the HTML for the Google homepage. 
-        }
-    })  
+// {
+//     measurement: "cpu_load",
+//     time: 1434311740594,
+//     value: 40.3
+//     tags:{"host":"kube_minion_3", core:"3"}
+// }
+
+export var formatPoint = function formatPoint(p) {
+
+    var kv_pairs = lo.map(p.tags, (v,k)=>{
+        return `${k}=${v}`
+    }).join(",")
+
+    return `${p.measurement},${kv_pairs} value=${p.value} ${p.time}`
 }
 
-var bodyCheck = function(body) {
-    console.log(body);
-    body = JSON.parse(body);
+export var put = function put(endpoint, database_name, points) {
+
+    if(!endpoint)       throw new Error("endpoint is required")
+    if(!database_name)  throw new Error("database_name is required")
+    if(!points)         throw new Error("points is required")
+
+    var body = lo.map(points, (p)=>{
+        return formatPoint(p);
+    }).join("\n")
+
+    // console.log("body");
+    // console.log(body);
+
+    var options = {
+        method: 'POST',
+        qs: {db: database_name, precision:"ms"},
+        url: `${endpoint}/write`,
+        resolveWithFullResponse: true,
+        body:body
+    }
+
+    return rp(options)
+    .then((request)=>{
+        bodyCheck(request.body)
+        return request;
+    })
+}
+
+var bodyCheck = function bodyCheck(body) {
+    if(body) {
+        body = JSON.parse(body);
+    } else {
+        return;
+    }
+
     if(body.results && body.results[0] && body.results[0].error) throw new Error(body.results[0].error);
 }
 
-export var create = function blah(endpoint, user, pass, name) {
+export var create = function create(endpoint, user, pass, name) {
 
     var options = {
         method: 'GET',
         uri: `${endpoint}/query?q=CREATE DATABASE ${name}`,
+        resolveWithFullResponse: true
     }
 
-    rp(options)
-    .then((body)=>{
-        console.log(body)
-    })
-    .catch((e)=>{
-        console.log(e)
+    return rp(options)
+    .then((request)=>{
+        bodyCheck(request.body)
+        return request;
     })
 }
 
-export var showMeasurements = function blah(endpoint, user, pass) {
+
+export var showMeasurements = function showMeasurements(endpoint, user, pass) {
 
 
     var options = {
@@ -39,12 +82,10 @@ export var showMeasurements = function blah(endpoint, user, pass) {
         uri: `${endpoint}/query?q=SHOW MEASUREMENTS`,
     }
 
-    rp(options)
-    .then((body)=>{
-        console.log(body)
-    })
-    .catch((e)=>{
-        console.log(e)
+    return rp(options)
+    .then((request)=>{
+        bodyCheck(request.body)
+        return request;
     })
 }
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -4,14 +4,77 @@ import * as influxdb from "../src/index";
 describe('Array', function(){
     
     describe('create()', function(){
-        it('should create a new influxdb database', function(){
+        it('should create a new influxdb database', function(done){
             influxdb.create("http://dockerbox:8086", null, null, "foottest")
+            .then(function(result){
+                console.log(JSON.stringify(result,true,4))
+                done();
+            })
+            .catch((e)=>{
+                done(e)
+            })
+        })
+    })
+
+    describe('formatPoint()', function(){
+        it('send in JSON point get out influxdb format line', function(){
+
+            var p = {
+                measurement: "cpu_load",
+                time: 1434311740594,
+                value: 40.3,
+                tags:{"host":"kube_minion_3", core:"3"}
+            }
+
+            var line = influxdb.formatPoint(p)
+            assert("cpu_load,host=kube_minion_3,core=3 value=40.3 1434311740594" === line)
+        })
+    })
+
+
+    describe('put()', function(){
+        it('add some data', function(done){
+
+
+            var p1 = {
+                measurement: "cpu_load",
+                time: 1434311740594,
+                value: 40,
+                tags:{"host":"kube_minion_3", core:"a3"}
+            }
+
+            var p2 = {
+                measurement: "cpu_load",
+                time: 1434313704006,
+                value: 12,
+                tags:{"host":"kube_minion_3", core:"a3"}
+            }
+
+            
+            var points = [p1,p2];
+
+            influxdb.put("http://dockerbox:8086", "foottest", points)
+            .then(function(result){
+                //console.log(result)
+                //console.log(result.statusCode)
+                //console.log(JSON.stringify(result,true,4))
+                done();
+            })
+            .catch((e)=>{
+                done(e)
+            })
         })
     }) 
      
-    describe('showMeasurements()', function(){
-        it('should create a new influxdb database', function(){
-            influxdb.showMeasurements("http://dockerbox:8086", null, null)
-        })
-    })
+    // describe('showMeasurements()', function(){
+    //     it('should create a new influxdb database', function(done){
+    //         influxdb.showMeasurements("http://dockerbox:8086", null, null)
+    //         .then(function(){
+    //             done();
+    //         })
+    //         .catch((e)=>{
+    //             done(e)
+    //         })
+    //     })
+    // })
 })
